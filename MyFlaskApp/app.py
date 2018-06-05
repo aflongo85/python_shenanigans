d38806f50f0579f6b3e9734788ec2084b8f0422d
Andrea Longo <anlongo@nodesagency.com>
2018-06-05 17:13:53 +0100

    changes in my flask app

--- MyFlaskApp/app.py
+++ MyFlaskApp/app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+my_stores = [{
+        'name': 'My Wonderful Store',
+        'id': '0001'
+    },
+    {
+        'name': 'My Super Market',
+        'id': '0002'
+    }
+]
+
+# class Person(object):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# a_new_person = Person("Andrea", 33)
 
 @app.route('/')
 def home():
@@ -14,9 +33,18 @@ def create_store():
 def get_store(name):
     return name
 
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': my_stores})
+    #return jsonify({'persona': a_new_person})
+
 @app.route('/store/<string:name>/item', methods=['POST'])
-def create_store(name):
+def create_store_with_name(name):
     pass
 
+#just test, not in the tutorial
+@app.route('/store/<string:name>/<string:item>')
+def create_store_with_name_and_item(name, item):
+    return "{} - {}".format(name, item)
 
-app.run(port=5000)
+app.run(port=8080)
